2c6d721139769a09534277ad38c199fa9f14099f
Alexander Borovskikh <borislav_god@mail.ru>
2019-01-25 22:14:21 +0500

    Описание логики и повидения для UI

---
+++ NewGaSApp/DicrementAndIncrimentButtonClickBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace NewGaSApp
+{
+    public class DicrementAndIncrimentButtonClickBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Click += OnClickHandler;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.Click -= OnClickHandler;
+        }
+
+        private void OnClickHandler(object sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject.Parent is StackPanel sp)
+                foreach (var child in sp.Children)
+                    if (child is TextBox tb)
+                    {
+                        string command = AssociatedObject.Tag.ToString().ToLower();
+                        int value = Int32.Parse(tb.Text);
+                        value = (command == "up") ? value + 1 : (command == "down") ? value - 1 : value;
+                        value = (value < 0) ? 0 : value;
+                        tb.Text = value.ToString();
+                    }
+        }
+    }
+}
--- NewGaSApp/MainWindowViewModel.cs
+++ NewGaSApp/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Mime;
-using System.Windows;
+﻿using System.Windows;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 
@@ -7,9 +6,32 @@ namespace NewGaSApp
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        #region Fields
+
         private string _title = "GaS Generation";
+        private RelayCommand _exitCommand;
+        private int _columns;
+        private int _rows;
+        private int _groups;
+        private string _result;
+        private bool _isDoubleSide;
+        private RelayCommand _resetCommand;
+        private RelayCommand _copyToCommand;
+
+        #endregion
+
+        #region Ctor
+
+        #endregion
+
+        #region Properties
 
         public const string TitlePropertyName = "Title";
+        public const string ColumnsPropertyName = "Columns";
+        public const string RowsPropertyName = "Rows";
+        public const string ResultPropertyName = "Result";
+        public const string GroupsPropertyName = "Groups";
+        public const string IsDoubleSidePropertyName = "IsDoubleSide";
 
         public string Title
         {
@@ -17,7 +39,51 @@ namespace NewGaSApp
             set { Set(TitlePropertyName, ref _title, value); }
         }
 
-        private RelayCommand _exitCommand;
+        public int Columns
+        {
+            get { return _columns; }
+            set
+            {
+                Set(ColumnsPropertyName, ref _columns, value);
+                UpdateGroupNumber();
+            }
+        }
+
+        public int Rows
+        {
+            get { return _rows; }
+            set
+            {
+                Set(RowsPropertyName, ref _rows, value);
+                UpdateGroupNumber();
+            }
+        }
+
+        public int Groups
+        {
+            get { return _groups; }
+            set { Set(GroupsPropertyName, ref _groups, value); }
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set { Set(ResultPropertyName, ref _result, value); }
+        }
+
+        public bool IsDoubleSide
+        {
+            get { return _isDoubleSide; }
+            set
+            {
+                Set(IsDoubleSidePropertyName, ref _isDoubleSide, value);
+                UpdateGroupNumber();
+            }
+        }
+
+        #endregion
+
+        #region Commands
 
         public RelayCommand ExitCommand
         {
@@ -29,5 +95,52 @@ namespace NewGaSApp
                 }));
             }
         }
+
+        public RelayCommand ResetCommand
+        {
+            get
+            {
+                return _resetCommand ?? (_resetCommand = new RelayCommand(Clear));
+            }
+        }
+
+        public RelayCommand CopyToCommand
+        {
+            get
+            {
+                return _copyToCommand ?? (_copyToCommand = new RelayCommand(() =>
+                {
+                    Result = "Copy to clipboard";
+                    Clipboard.SetText(Result);
+                }));
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        private void Clear()
+        {
+            Columns = 0;
+            Rows = 0;
+            Result = "";
+            IsDoubleSide = false;
+        }
+
+        private void UpdateGroupNumber()
+        {
+            int g = Columns * Rows;
+            Groups = (IsDoubleSide) ? g * 2 : g;
+        }
+        #endregion
+
+        #region Exceptions classes
+
+        #endregion
     }
 }
